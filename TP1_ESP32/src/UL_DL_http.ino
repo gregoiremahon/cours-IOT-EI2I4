0d4565fd33d2cc7770ab8744fb42ca8bcb91587c
gregoiremahon <greg.mahondu78@gmail.com>
2023-12-19 13:20:49 +0100

    Commentaires code http

--- TP1_ESP32/src/UL_DL_http.ino
+++ TP1_ESP32/src/UL_DL_http.ino
@@ -2,9 +2,9 @@
  * UL_DL_http.ino
  * Authors: Grégoire MAHON & Armand LELONG [EI2I 4 II - Groupe B]
  *
- * This code connects an ESP32 to a WiFi network and sends sensor data (temperature
- * and humidity) to a Node-RED server using HTTP RESTful services. It also requests
- * the current state of an LED from the server and controls the LED accordingly.
+ * This code connects an ESP32 to a WiFi network and sends temperature and humidity data
+ * from a DHT11 sensor to a Node-RED server using HTTP POST. It also checks the state of an LED
+ * via an HTTP GET request and controls the LED based on the server's response.
  */
 
 #include "DHT.h"
@@ -20,6 +20,7 @@ const char* password = "PASSWORD";
 
 DHT dht(DHTPIN, DHTTYPE);
 
+// Connects to the WiFi network
 void connectToWiFi() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -27,6 +28,7 @@ void connectToWiFi() {
   }
 }
 
+// Reads data from DHT sensor and sends it to the server
 void readSensorAndSendData() {
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
@@ -40,17 +42,18 @@ void readSensorAndSendData() {
     char jsonBuffer[512];
     serializeJson(jsonDoc, jsonBuffer);
     HTTPClient http;
-    http.begin("http://172.20.10.3:1880/dht11");
+    http.begin("http://172.20.10.3:1880/dht11");  // Server URL
     http.addHeader("Content-Type", "application/json");
     http.POST(jsonBuffer);
     http.end();
   }
 }
 
+// Checks the LED state from the server and controls the LED
 void getLedState() {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
-    http.begin("http://172.20.10.3:1880/ledState");
+    http.begin("http://172.20.10.3:1880/ledState");  // URL for LED state
     if (http.GET() == 200) {
       String response = http.getString();
       digitalWrite(LED_BUILTIN, response == "true" ? HIGH : LOW);
